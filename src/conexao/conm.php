e4aa018f101120ed0a6592c3b82bf946fabd5715
danielgomes775 <danielgcmenezes2021@gmail.com>
2022-06-07 22:41:01 -0300

    Banco conectado, tela lista feita.

--- src/conexao/conm.php
+++ src/conexao/conm.php
@@ -1,7 +1,15 @@
 <?php
     //Carregar as credenciais do banco de dados
-    $hostname = "";
-    $database = "";
-    $user = "";
-    $password = "";
+    $hostname = "sql103.epizy.com";
+    $database = "epiz_31493722_sysrifa";
+    $user = "epiz_31493722";
+    $password = "Ksge5RmV1KTY";
+
+    try {
+        $pdo = new PDO('mysql:host='.$hostname.';dbname='.$database, $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'Conexão com o banco de dados '.$database.', foi realizada com sucesso';
+    } catch (PDOException $e){
+        echo 'Erro: ' .$e->getMessage();
+    }
  
